d3e9e82c7249e9acffc32598f56167807179dcd8
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-05-13 14:12:21 -0700

    Added function to correctly display city name

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var APIkey = "cc11cb432484c7170f84edd3a05552d8";
 
 var searchBtn = document.querySelector("#search-btn");
 
-var searchHistory = document.querySelector("search-history"); 
-var currentWeatherCard = document.querySelector("#current-weather");
+var searchHistory = document.querySelector("#search-history"); 
+var currentWeatherBody = document.querySelector(".card-body");
 var fiveDayForecast = document.querySelector("#five-day-forecast"); 
 
 searchBtn.addEventListener("click", callWeatherApi);
@@ -27,7 +27,8 @@ function callWeatherApi() {
       console.log(data);
       let lat = data[0].lat
       let long = data[0].lon
-      console.log(lat, long);
+      let cityName = data[0].name 
+      displayCityName(cityName);
 
       let oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&exclude=hourly,minutely&appid=" + APIkey;
 
@@ -47,42 +48,22 @@ function callWeatherApi() {
     );
     // Add user search to history list if something is found 
     let userSearchHistory = document.createElement("li"); 
-    userSearchHistory.textContent = userSearch
+    userSearchHistory.textContent = userSearch; 
     searchHistory.append(userSearchHistory); 
 
 }
 
+function displayCityName(name) {
+  currentWeatherBody.innerHTML = ""; 
+  let cityNameEl = document.createElement("h2"); 
+  cityNameEl.classList.add("card-title"); 
+  cityNameEl.textContent = name; 
+  console.log(cityNameEl)
+  currentWeatherBody.append(cityNameEl);  
+}
+
 function printCurrent(currentWeatherResult) {
   console.log(currentWeatherResult);
-
-  let currentWeatherBody = document.createElement("div");
-  currentWeatherBody.classList.add("card-body");
-  currentWeatherBody.append(currentWeatherCard);
-
-  let userSearch = document.querySelector("#location-search").value;
-  let currentTitle = document.createElement("h2");
-  currentTitle.classList.add("card-title"); 
-  currentTitle.textContent = userSearch; 
-  console.log(currentTitle);
-  currentWeatherBody.append(currentTitle); 
-
-//   let coordinateUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + userSearch + "&limit=1&appid=" + APIkey;
-
-//   console.log(coordinateUrl);
-
-//   fetch(coordinateUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-
-//       let currentTitle = document.createElement("h2");
-//       currentTitle.classList.add("card-title"); 
-//       currentTitle.textContent = data[0].name; 
-//       console.log(currentTitle);
-//       currentWeatherBody.append(currentTitle); 
-// })
   
   let currentTemp = document.createElement("p"); 
   currentTemp.classList.add("card-text"); 
@@ -92,7 +73,7 @@ function printCurrent(currentWeatherResult) {
 
   // preliminary five day forecast draft 
   for (let i = 0; i < currentWeatherResult.daily.length; i++) {
-    
+
   }
 }
 
